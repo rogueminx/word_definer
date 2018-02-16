8c220903ac14aee018fec6dea05e5293545e164b
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-02-16 08:59:21 -0800

    write first spec with meaningful fail

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,7 +1,22 @@
 class Word
+  @@word_list = []
 
-  def initialize()
+  attr_accessor :word
+  attr_accessor :definition
+
+  def initialize(word, definition)
+    @word = word
+    @@defintion_list = [definition]
+  end
+
+  def self.all()
+    # @@word_list
+  end
+
+
+  def save()
 
   end
+
 
 end
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -4,10 +4,20 @@ require('word_definer')
 
 describe("Word")do
 
-  describe("") do
-    it("") do
-
+  describe(".all") do
+    it("is empty at first, and will populate later") do
+      expect(Word.all()).to(eq([]))
     end
   end
+
+
+  # describe("#save_word") do
+  #   it("saves a new word") do
+  #     new_word = Word.new("frog")
+  #     new_word.save()
+  #     expect(Word.)
+  #
+  #   end
+  # end
 
 end
